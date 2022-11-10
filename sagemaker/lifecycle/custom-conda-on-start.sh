dd37686c9a40520a804dc5fa7c44e02098b87a37
Dmytro Yurchenko <dmytro.yurchenko@homegate.ch>
2022-11-10 18:53:15 +0100

    fix: git config

--- sagemaker/lifecycle/custom-conda-on-start.sh
+++ sagemaker/lifecycle/custom-conda-on-start.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 set -euxo pipefail
+########################## Git setup ###########################################
+GIT_USER=$GIT_USER
+GIT_EMAIL=$GIT_EMAIL
+sudo -u ec2-user -E git config --global user.email "${GIT_EMAIL}"
+sudo -u ec2-user -E git config --global user.name "${GIT_USER}"
+sudo -u ec2-user -E git config --global pull.rebase true
+sudo -u ec2-user -E git config --global alias.a add
+sudo -u ec2-user -E git config --global alias.b branch
+sudo -u ec2-user -E git config --global alias.c commit
+sudo -u ec2-user -E git config --global alias.cl clone
+sudo -u ec2-user -E git config --global alias.co checkout
+sudo -u ec2-user -E git config --global alias.cp cherry-pick
+sudo -u ec2-user -E git config --global alias.m merge
+sudo -u ec2-user -E git config --global alias.p push --follow-tags
+sudo -u ec2-user -E git config --global alias.pu pull
+sudo -u ec2-user -E git config --global alias.r reset
+sudo -u ec2-user -E git config --global alias.s status
+sudo -u ec2-user -E git config --global alias.force-push push --force-with-lease
+sudo -u ec2-user -E git config --global alias.fp force-push
+sudo -u ec2-user -E git config --global init.defaultbranch main
+################################################################################
 # OVERVIEW
 # This script installs a custom, persistent installation of conda on the Notebook Instance's EBS volume, and ensures
 # that these custom environments are available as kernels in Jupyter.
@@ -14,21 +35,13 @@ set -euxo pipefail
 # https://docs.aws.amazon.com/sagemaker/latest/dg/nbi-add-external.html#nbi-isolated-environment
 export WORKING_DIR=/home/ec2-user/SageMaker/custom-miniconda
 export PATH=$WORKING_DIR/miniconda/bin:$PATH
-export GIT_USER=$GIT_USER
-export GIT_EMAIL=$GIT_EMAIL
+
 sudo -n -u ec2-user -i <<'EOF'
 set -euxo pipefail
 unset SUDO_UID
 
 WORKING_DIR=/home/ec2-user/SageMaker/custom-miniconda
 PATH=$WORKING_DIR/miniconda/bin:$PATH
-GIT_USER=$GIT_USER
-GIT_EMAIL=$GIT_EMAIL
-
-git config --global user.email "${GIT_EMAIL}"
-git config --global user.name "${GIT_USER}"
-git config --global pull.rebase true
-
 source "$WORKING_DIR/miniconda/bin/activate"
 
 for env in $WORKING_DIR/miniconda/envs/*; do
@@ -36,7 +49,8 @@ for env in $WORKING_DIR/miniconda/envs/*; do
 BASENAME=$(basename "$env")
 source activate "$BASENAME"
 
-python -m ipykernel install --user --name "$BASENAME" --display-name "Python (${BASENAME})" --sys-prefix --env PATH "${PATH}"
+python -m ipykernel install --user --name "$BASENAME" \
+  --display-name "Python (${BASENAME})" --sys-prefix --env PATH "${PATH}"
 done
 # Optionally, uncomment these lines to disable SageMaker-provided Conda functionality.
 
