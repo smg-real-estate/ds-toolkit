68d2b38877fe8807c1a16c5858145d3cf84b1c06
Dmytro Yurchenko <dmytro.yurchenko@homegate.ch>
2023-04-25 09:12:13 +0200

    Move auto-stop part to the top of the script

--- sagemaker/lifecycle/custom-conda-on-start.sh
+++ sagemaker/lifecycle/custom-conda-on-start.sh
@@ -22,6 +22,32 @@ sudo -u ec2-user -E git config --global alias.fp force-push
 sudo -u ec2-user -E git config --global init.defaultbranch main
 ################################################################################
 
+# OVERVIEW
+# This part of the script stops a SageMaker notebook once it's idle for more than 1 hour (default time)
+# You can change the idle time for stop using the environment variable below.
+# If you want the notebook the stop only if no browsers are open, remove the --ignore-connections flag
+#
+# Note that this script will fail if either condition is not met
+#   1. Ensure the Notebook Instance has internet connectivity to fetch the example config
+#   2. Ensure the Notebook Instance execution role permissions to SageMaker:StopNotebookInstance to stop the notebook 
+#       and SageMaker:DescribeNotebookInstance to describe the notebook.
+
+PROJECT_URL=$PROJECT_URL
+BRANCH=$BRANCH
+EC2_HOME=/home/ec2-user
+echo "source $EC2_HOME/SageMaker/ds-toolkit/sagemaker/lifecycle/bashrc" >> $EC2_HOME/.profile
+IDLE_TIME=7200
+CONDA_ENV_NAME=smg-re-py3.7
+CONDA_ENV_PATH=${EC2_HOME}/SageMaker/.kernels/${CONDA_ENV_NAME}
+echo "Fetching the autostop script"
+wget ${PROJECT_URL}/${BRANCH}/sagemaker/lifecycle/autostop.py \
+  -O ${EC2_HOME}/autostop.py
+
+echo "Starting the SageMaker autostop script in cron"
+(crontab -l 2>/dev/null; echo "*/5 * * * * ${CONDA_ENV_PATH}/bin/python ${EC2_HOME}/autostop.py --time ${IDLE_TIME} --ignore-connections") | crontab -
+
+crontab -l
+
 ##################### script that is run under ec2-user ########################
 sudo -n -u ec2-user -i <<'EOF'
 set -euxo pipefail
@@ -66,28 +92,4 @@ EOF
 echo "Restarting the Jupyter server.."
 systemctl restart jupyter-server
 
-# OVERVIEW
-# This part of the script stops a SageMaker notebook once it's idle for more than 1 hour (default time)
-# You can change the idle time for stop using the environment variable below.
-# If you want the notebook the stop only if no browsers are open, remove the --ignore-connections flag
-#
-# Note that this script will fail if either condition is not met
-#   1. Ensure the Notebook Instance has internet connectivity to fetch the example config
-#   2. Ensure the Notebook Instance execution role permissions to SageMaker:StopNotebookInstance to stop the notebook 
-#       and SageMaker:DescribeNotebookInstance to describe the notebook.
 
-PROJECT_URL=$PROJECT_URL
-BRANCH=$BRANCH
-EC2_HOME=/home/ec2-user
-echo "source $EC2_HOME/SageMaker/ds-toolkit/sagemaker/lifecycle/bashrc" >> $EC2_HOME/.profile
-IDLE_TIME=7200
-CONDA_ENV_NAME=smg-re-py3.7
-CONDA_ENV_PATH=${EC2_HOME}/SageMaker/.kernels/${CONDA_ENV_NAME}
-echo "Fetching the autostop script"
-wget ${PROJECT_URL}/${BRANCH}/sagemaker/lifecycle/autostop.py \
-  -O ${EC2_HOME}/autostop.py
-
-echo "Starting the SageMaker autostop script in cron"
-(crontab -l 2>/dev/null; echo "*/5 * * * * ${CONDA_ENV_PATH}/bin/python ${EC2_HOME}/autostop.py --time ${IDLE_TIME} --ignore-connections") | crontab -
-
-crontab -l
