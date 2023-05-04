8e4aa4c40be04274889289d89425a65d792db7c5
Dmytro Yurchenko <dmytro.yurchenko@swissmarketplace.group>
2023-05-04 16:48:32 +0200

    fix: move conda environment creation into a separate script

--- sagemaker/lifecycle/studio-jupyter-server-on-start.sh
+++ sagemaker/lifecycle/studio-jupyter-server-on-start.sh
@@ -76,60 +76,6 @@ if [ ! -d $HOME/.micromamba ]; then
     && popd
 fi
 
-if [ -d $HOME/ds-toolkit ]; then
-  pushd $HOME/ds-toolkit && git pull && popd
-  cp $HOME/ds-toolkit/sagemaker/lifecycle/bashrc-studio.sh $HOME/.micromamba/bashrc-studio.sh
-  source $HOME/.micromamba/bashrc-studio.sh
-
-  micromamba config append channels conda-forge
-  micromamba config append channels defaults
-  micromamba config append envs_dirs $HOME/.conda/envs
-  micromamba config append envs_dirs $KERNELS_DIR
-
-  ################### Create conda environments #######################
-  KERNEL_NAME="smg-re"
-  PYTHON_VERSIONS=("3.9" "3.10")
-
-  for python_version in ${PYTHON_VERSIONS[@]}; do
-      ENV_NAME="${KERNEL_NAME}-py${python_version}"
-      micromamba create -q -y python=${python_version} \
-        -p $KERNELS_DIR/$ENV_NAME \
-        ipykernel watchtower urllib3[secure] requests pre-commit nbdime -c conda-forge
-
-      micromamba run -r $KERNELS_DIR/$ENV_NAME \
-        python -m ipykernel install --user --name "$ENV_NAME" \
-        --display-name "Python (${ENV_NAME})"
-  done
-
-  ################### Install pre-commit hooks ########################
-  cd ${HOME}
-
-  py310_projects=("ds-projects")
-
-  for project in ${py310_projects[@]}; do
-    if [ -d $project ]; then
-        pushd $project
-        if [ -e .pre-commit-config.yaml ]; then
-          micromamba run -r "${KERNELS_DIR}/${KERNEL_NAME}-py3.10" \
-            pre-commit install --install-hooks  
-        fi
-        popd
-    fi
-  done
-
-  py39_projects=("managed-airflow" "data-platform")
-
-  for project in ${py39_projects[@]}; do
-    if [ -d $project ]; then
-        pushd $project
-        if [ -e .pre-commit-config.yaml ]; then
-          micromamba run -r "${KERNELS_DIR}/${KERNEL_NAME}-py3.9" \
-            pre-commit install --install-hooks  
-        fi
-        popd
-    fi
-  done
-fi
 ################### Configure Auto-shutdown #########################
 # timeout in minutes
 export TIMEOUT_IN_MINS=120
