c00f759711fce4f7b3c18fb6b01b715268739973
Dmytro Yurchenko <dmytro.yurchenko@swissmarketplace.group>
2023-05-04 16:18:53 +0200

    fix: kernel gateway activation LCC

--- sagemaker/lifecycle/studio-kernel-gateway-setup-conda.sh
+++ sagemaker/lifecycle/studio-kernel-gateway-setup-conda.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
 set -eux
+ln -sf /root /home/sagemaker-user
 
 PRE_COMMIT_HOME=${HOME}/.cache/pre-commit
 KERNELS_DIR=${HOME}/.kernels
 
-pushd $HOME/ds-toolkit && git pull && popd
-source $HOME/ds-toolkit/sagemaker/lifecycle/bashrc-studio.sh
-
-micromamba config append channels conda-forge
-micromamba config append channels defaults
-micromamba config append envs_dirs $HOME/.conda/envs
-micromamba config append envs_dirs $KERNELS_DIR
+if [ ! -d $HOME/.micromamba ]; then
+  source $HOME/.micromamba/bashrc-studio.sh
+fi
 
 for env in $KERNELS_DIR/*; do
     micromamba run -r $KERNELS_DIR/$env \
       python -m ipykernel install --user --name "$BASENAME" \
       --display-name "Python (${BASENAME})"
 done
-nohup supervisorctl -c /etc/supervisor/conf.d/supervisord.conf restart jupyterlabserver
