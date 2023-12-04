d09d4086d9b53a388eb88ec67357687a7aa1bb0a
Dmytro Yurchenko <dmytro.yurchenko@swissmarketplace.group>
2023-12-04 16:39:43 +0100

    DAB-1528 feat: add JSON support to S3 utils

--- ds_toolkit/utils.py
+++ ds_toolkit/utils.py
@@ -1,5 +1,7 @@
+import json
 import pickle
-from io import BytesIO
+from enum import Enum
+from io import BytesIO, StringIO
 from typing import Any
 
 from botocore.client import BaseClient
@@ -12,7 +14,22 @@ def send_datadog_metric(options, *args, **kwargs):
     Metric.send(*args, **kwargs)
 
 
-def dump_object_to_s3(client: BaseClient, obj: Any, bucket: str, key: str):
+class ObjectFormat(Enum):
+    """
+    Enum to define the format of the object
+    """
+
+    PICKLE = "pickle"
+    JSON = "json"
+
+
+def dump_object_to_s3(
+    client: BaseClient,
+    obj: Any,
+    bucket: str,
+    key: str,
+    format: ObjectFormat = ObjectFormat.PICKLE,
+):
     """
     Dump an object to S3
 
@@ -20,10 +37,17 @@ def dump_object_to_s3(client: BaseClient, obj: Any, bucket: str, key: str):
     :param obj: Object to dump
     :param bucket: S3 bucket
     :param key: S3 key
+    :param format: Format of the object, can be pickle or json
     :return: None
     """
-    buff = BytesIO()
-    buff.write(pickle.dumps(obj))
+    if format == ObjectFormat.PICKLE:
+        buff = BytesIO()
+        buff.write(pickle.dumps(obj))
+    elif format == ObjectFormat.JSON:
+        buff = StringIO()
+        json.dump(obj, buff)
+    else:
+        raise ValueError(f"Unknown format: {format}")
     buff.seek(0)
 
     client.put_object(
@@ -33,17 +57,26 @@ def dump_object_to_s3(client: BaseClient, obj: Any, bucket: str, key: str):
     )
 
 
-def load_object_from_s3(client: BaseClient, bucket: str, key: str) -> Any:
+def load_object_from_s3(
+    client: BaseClient,
+    bucket: str,
+    key: str,
+    format: ObjectFormat = ObjectFormat.PICKLE,
+) -> Any:
     """
     Load an object from S3
 
     :param client: S3 client
     :param bucket: S3 bucket
     :param key: S3 key
+    :param format: Format of the object, can be pickle or json
     :return: Loaded object
     """
     buff = client.get_object(
         Bucket=bucket,
         Key=key,
     )["Body"].read()
-    return pickle.loads(buff)
+    if format == ObjectFormat.PICKLE:
+        return pickle.loads(buff)
+    elif format == ObjectFormat.JSON:
+        return json.loads(buff)
